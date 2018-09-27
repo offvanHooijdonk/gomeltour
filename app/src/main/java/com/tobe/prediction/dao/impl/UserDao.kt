280d2f4950713261a70dc16c0c81c65d96ba9229
Yahor Fralou <yahor.fralou@kronos.com>
2018-09-27 11:54:49 +0300

    Fixing Dao injection

--- app/src/main/java/com/tobe/prediction/dao/impl/UserDao.kt
+++ app/src/main/java/com/tobe/prediction/dao/impl/UserDao.kt
@@ -1,21 +1,18 @@
 package com.tobe.prediction.dao.impl
 
 import com.google.firebase.firestore.CollectionReference
-import com.google.firebase.firestore.FirebaseFirestore
 import com.tobe.prediction.dao.IUserDao
 import com.tobe.prediction.dao.converter.convertUser
 import com.tobe.prediction.domain.UserBean
 import durdinapps.rxfirebase2.RxFirestore
 import io.reactivex.Maybe
+import javax.inject.Inject
 
 /**
  * Created by Yahor_Fralou on 9/26/2018 2:53 PM.
  */
 
-class UserDao /*@Inject*/ constructor() : IUserDao {
-    /*@Inject
-    @Named(DM.Names.REF_USERS)*/
-    /*lateinit*/ var refUsers: CollectionReference = FirebaseFirestore.getInstance().collection("users")
+class UserDao @Inject constructor(var refUsers: CollectionReference) : IUserDao {
 
     override fun save(user: UserBean) =
         RxFirestore.setDocument(refUsers.document(user.id), user)
