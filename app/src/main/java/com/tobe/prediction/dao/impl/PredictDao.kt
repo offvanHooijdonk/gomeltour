7a85ad7d14e96a76de12b81c4dbd4e18678b0e28
Yahor Fralou <yahor.fralou@kronos.com>
2018-10-05 18:27:39 +0300

    Making 'options' saving into collections - hardcoded

--- app/src/main/java/com/tobe/prediction/dao/impl/PredictDao.kt
+++ app/src/main/java/com/tobe/prediction/dao/impl/PredictDao.kt
@@ -6,6 +6,7 @@ import com.tobe.prediction.domain.Predict
 import durdinapps.rxfirebase2.RxFirestore
 import io.reactivex.Completable
 import io.reactivex.Maybe
+import java.util.*
 import javax.inject.Inject
 
 /**
@@ -17,10 +18,27 @@ class PredictDao @Inject constructor(var refPredict: CollectionReference) : IPre
     override fun save(predict: Predict): Completable {
         val id = refPredict.document().id
         predict.id = id
-        return RxFirestore.setDocument(refPredict.document(id), predict)
+        val predictDoc = PredictDoc(predict.id, predict.title, predict.text, predict.dateOpenTill, predict.dateFulfillment, predict.isActive, predict.userId)
+        return RxFirestore.setDocument(refPredict.document(id), predictDoc) // todo try addDocument
+                .mergeWith(
+                        RxFirestore.setDocument(refPredict.document(id).collection("options").document("0"), OptionDoc(0, predict.options[0]))
+                )
+                .mergeWith(
+                        RxFirestore.setDocument(refPredict.document(id).collection("options").document("1"), OptionDoc(1, predict.options[1]))
+                )
     }
 
     override fun list(): Maybe<MutableList<Predict>> {
         return RxFirestore.getCollection(refPredict, Predict::class.java)
     }
+
+    private data class PredictDoc(var id: String = "",
+                                  var title: String = "",
+                                  var text: String = "",
+                                  var dateOpenTill: Date = Date(),
+                                  var dateFulfillment: Date = Date(),
+                                  var isActive: Boolean = true,
+                                  var userId: String = "") // todo reference
+
+    private data class OptionDoc(var code: Int = -1, var text: String = "")
 }
